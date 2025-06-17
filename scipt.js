b914f1f48588ea62b5037714226f2d11e52af8ba
Alicemuthoni-dr <muthonimaker@gmail.com>
2025-06-17 16:37:48 +0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,34 @@
+document.addEventListenerZ("DOMContentLoaded",()  =>{
+    const form=
+    document.getElementById("taskForm");
+    const categoryFilter =
+    document.getElementById("filter Category");
+    const priorityFilter =
+    document.getElementById("filter priority");
+
+    const tasklist =
+    document.createElement("ul");
+    document.body.appendChild(tasklist);
+
+    form.addEventListener("submit" , function (e)  {
+        e.preventDefault() ;
+
+    const title =
+    document.getElementById("title").value;
+    const desc =
+    document.getElementById("description").value;
+
+const category =
+document.getElementById("category").value;
+const priority =
+document.getElementById("priority").value;
+const dueDate =
+document.getElementById("dueDate").value;
+
+const li =
+document.createElement("li");
+li.textContent =`${title}  - ${desc} [${category} , ${priority} , Due:${dueDate}]`;
+tasklist.appendChild(li);
+form.reset();
+});
+});
